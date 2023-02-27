2e981f630c353ce7a5bc222880562d01f49de192
betul <ksraysebtl@gmail.com>
2023-02-27 18:30:51 +0100

    Test Uploaded

--- src/Test/Test4.java
+++ src/Test/Test4.java
@@ -3,5 +3,6 @@ package Test;
 public class Test4 {
     public static void main(String[] args) {
         System.out.println("Test 4");
+        System.out.println("Test 2");
     }
 }
--- src/Test/YeniTest.java
+++ src/Test/YeniTest.java
@@ -3,4 +3,7 @@ package Test;
 public class YeniTest {
     // local test branch da eklendi
     // Ahanda bu satir remote dan yani github dan eklendi!!:)
+    public static void main(String[] args) {
+        System.out.println("Yeni Test");
+    }
 }
